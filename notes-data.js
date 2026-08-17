f781ce4efa0467d0a989835f499a3967e402ed6f
Sarthak Roy_tech <178605815+SarthakRoy05@users.noreply.github.com>
2026-08-18 00:30:34 +0530

    new vide,note add

--- notes-data.js
+++ notes-data.js
@@ -7,9 +7,16 @@ const NOTES_SUBJECTS = [
     topics: [
       {
         id: "java-1",
-        title: "Module 1",
+        title: "Module 1_Short Notes",
         notes: [
-          { title: "Module 1 Notes", file: "notes/java/Java_Module1.pdf" }
+          { title: "Module 1 Short Notes", file: "notes/java/Java_Module1.pdf" }
+        ]
+      },
+      {
+        id: "java-1",
+        title: "Module 1_Long Notes",
+        notes: [
+          { title: "Module 1 Long Notes", file: "notes/java/Java Unit 1 - BCA 5th Sem.pdf" }
         ]
       }
     ]
